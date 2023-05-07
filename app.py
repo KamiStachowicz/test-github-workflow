0cffeae97f735277446bd45c2920dcff9e695aa2
KamiStachowicz <75569396+KamiStachowicz@users.noreply.github.com>
2023-05-07 17:36:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
+"""
+Module with a simple Flask application.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Return a simple HTML page.
+
+    Returns:
+    -------
+    str
+        HTML content to be displayed.
+    """
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
